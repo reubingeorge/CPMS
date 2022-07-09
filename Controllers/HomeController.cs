e27e19a8f53f078ed3ca83f65ee035f0a8ceccba
andrictam <50673845+andrictam@users.noreply.github.com>
2022-07-09 08:04:37 -0400

    Cookie authentication, register system

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using CPMS.Data;
 using CPMS.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 
 namespace CPMS.Controllers
@@ -15,20 +19,8 @@ namespace CPMS.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
-        {
-            if (HttpContext.Session.GetInt32("validAuthor") != null || 
-                HttpContext.Session.GetInt32("validReviewer") != null)
-            {
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("Login");
-            }
-        }
-
-        public IActionResult Register()
         {
             return View();
         }
@@ -40,40 +32,127 @@ namespace CPMS.Controllers
 
         // NOTE: the button decider method within only works in English.
         // The specific value of buttonPress depends on which button is pressed in Login.cshtml.
-        // The basis of it is that only one button can be pressed before redirection, so whichever
-        // value was passed in must be the button that was pressed.
-        public IActionResult Login(string email, string password, string buttonPress)
+        // The basis of it is that only one of the two identically named buttons can be pressed before redirection,
+        // so whichever value was passed in must be the button that was pressed.
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string email, string password, string buttonPress)
         {
-            // Checks that all function arguments are not empty
+            // No model, checks that all function arguments are not empty
             if (ModelState.IsValid)
             {
-                // String check - this is where translated page would fail
-                if (buttonPress.Equals("Author Login")) 
+                // admin can use either button to log in
+                if (email.Equals("admin@cpms.org") && password.Equals("12345"))
                 {
-                    AuthorDAO authorDAO = new();
-                    if (authorDAO.LoginCheck(email, password))
+                    var claims = new List<Claim>() {
+                            new Claim(ClaimTypes.Name, "Admin"),
+                            new Claim(ClaimTypes.Role, "Admin")
+                        };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                    return RedirectToAction("Index");
+                }
+                //String check -this is where translated page would fail
+                else if (buttonPress.Equals("Author Login"))
+                {
+                    AuthorDAO authorDataAccess = new();
+                    if (authorDataAccess.LoginCheck(email, password))
                     {
-                        HttpContext.Session.SetInt32("validAuthor", 1); // Set as int but value is not checked later
+                        // Generate claims
+                        var claims = new List<Claim>() {
+                            new Claim(ClaimTypes.Name, email),
+                            new Claim(ClaimTypes.Role, "Author")
+                        };
+
+                        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
                         return RedirectToAction("Index");
                     }
                     else
                     {
                         ViewBag.error = "Login failed";
-                        return RedirectToAction("Login");
+                        return RedirectToAction("Index");
                     }
                 }
-                else  // for reviewers, no code yet because reviewer data class has not been made 
+                else
                 {
-                    HttpContext.Session.SetInt32("validReviewer", 1);
-                    return RedirectToAction("Index");
+                    ReviewerDAO reviewerDataAccess = new();
+                    if (reviewerDataAccess.LoginCheck(email, password))
+                    {
+                        // Generate claims
+                        var claims = new List<Claim>() {
+                            new Claim(ClaimTypes.Name, email),
+                            new Claim(ClaimTypes.Role, "Reviewer")
+                        };
+
+                        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.error = "Login failed";
+                        return RedirectToAction("Index");
+                    }
                 }
             }
             return View();
         }
 
-        public IActionResult Logout()
+        [AllowAnonymous]
+        public IActionResult RegisterSelect(string buttonPress)
         {
-            HttpContext.Session.Clear();    
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> RegisterAuthor(AuthorModel author)
+        {
+            if (ModelState.IsValid)
+            {
+                AuthorDAO authorDataAccess = new();
+                if (authorDataAccess.LoginCheck(author.Email, author.Password))
+                {
+                    
+                }
+                authorDataAccess.CreateOrUpdate(author);
+
+                await Login(author.Email, author.Password, "Author Login");
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> RegisterReviewer(ReviewerModel reviewer)
+        {
+            if (ModelState.IsValid)
+            {
+                ReviewerDAO reviewerDataAccess = new();
+                if (reviewerDataAccess.LoginCheck(reviewer.Email, reviewer.Password))
+                {
+
+                }
+                reviewerDataAccess.CreateOrUpdate(reviewer);
+
+                await Login(reviewer.Email, reviewer.Password, "reviewer Login");
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login");
         }
 
