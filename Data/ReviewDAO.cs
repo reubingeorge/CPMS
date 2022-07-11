f050cb5b8b2def5628f2dba1a19e11eecdc5e97b
andrictam <50673845+andrictam@users.noreply.github.com>
2022-07-10 22:28:16 -0400

    Added ability to download reports

---
+++ Data/ReviewDAO.cs
@@ -0,0 +1,63 @@
+﻿using CPMS.Models;
+using System.Data.SqlClient;
+
+namespace CPMS.Data
+{
+    internal class ReviewDAO
+    {
+        private string connectionString =
+            @"Data Source=(localdb)\ProjectModels;
+                Initial Catalog=CPMS;
+                Integrated Security=True;
+                Connect Timeout=30;Encrypt=False;
+                TrustServerCertificate=False;
+                ApplicationIntent=ReadWrite;
+                MultiSubnetFailover=False";
+
+        public List<ReviewModel> FetchAll()
+        {
+            List<ReviewModel> reviewList = new();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM dbo.Review";
+                SqlCommand sqlCommand = new(sqlQuery, sqlConnection);
+                sqlConnection.Open();
+
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        ReviewModel reviewModel             = new();
+                        reviewModel.ReviewID                = dataReader.GetInt32(0);
+                        reviewModel.PaperID                 = dataReader.IsDBNull(1) ? null : dataReader.GetInt32(1);
+                        reviewModel.ReviewerID              = dataReader.IsDBNull(2) ? null : dataReader.GetInt32(2);
+                        reviewModel.AppropriatenessOfTopic  = dataReader.IsDBNull(3) ? null : dataReader.GetDecimal(3);
+                        reviewModel.TimelinessOfTopic       = dataReader.IsDBNull(4) ? null : dataReader.GetDecimal(4);
+                        reviewModel.SupportiveEvidence      = dataReader.IsDBNull(5) ? null : dataReader.GetDecimal(5);
+                        reviewModel.TechnicalQuality        = dataReader.IsDBNull(6) ? null : dataReader.GetDecimal(6);
+                        reviewModel.ScopeOfCoverage         = dataReader.IsDBNull(7) ? null : dataReader.GetDecimal(7);
+                        reviewModel.CitationOfPreviousWork  = dataReader.IsDBNull(8) ? null : dataReader.GetDecimal(8);
+                        reviewModel.Originality             = dataReader.IsDBNull(9) ? null : dataReader.GetDecimal(9);
+                        reviewModel.ContentComments         = dataReader.IsDBNull(10) ? null : dataReader.GetString(10);
+                        reviewModel.OrganizationOfPaper     = dataReader.IsDBNull(11) ? null : dataReader.GetDecimal(11);
+                        reviewModel.ClarityOfMainMessage    = dataReader.IsDBNull(12) ? null :dataReader.GetDecimal(12);
+                        reviewModel.Mechanics               = dataReader.IsDBNull(13) ? null : dataReader.GetDecimal(13);
+                        reviewModel.WrittenDocumentComments = dataReader.IsDBNull(14) ? null : dataReader.GetString(14);
+                        reviewModel.SuitabilityForPresentation = dataReader.IsDBNull(15) ? null : dataReader.GetDecimal(15);
+                        reviewModel.PotentialInterestInTopic = dataReader.IsDBNull(16) ? null : dataReader.GetDecimal(16);
+                        reviewModel.PotentialForOralPresentationComments = dataReader.IsDBNull(17) ? null : dataReader.GetString(17);
+                        reviewModel.OverallRating           = dataReader.IsDBNull(18) ? null : dataReader.GetDecimal(18);
+                        reviewModel.OverallRatingComments   = dataReader.IsDBNull(19) ? null : dataReader.GetString(19);
+                        reviewModel.ComfortLevelTopic       = dataReader.IsDBNull(20) ? null : dataReader.GetDecimal(20);
+                        reviewModel.ComfortLevelAcceptability = dataReader.IsDBNull(21) ? null : dataReader.GetDecimal(21);
+                        reviewModel.Complete = dataReader.IsDBNull(22) ? null : dataReader.GetBoolean(22);
+
+                        reviewList.Add(reviewModel);
+                    }
+                }
+            }
+            return reviewList;
+        }
+    }
+}
--- Models/ReviewModel.cs
+++ Models/ReviewModel.cs
@@ -1,7 +1,119 @@
-﻿namespace CPMS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPMS.Models
 {
     public class ReviewModel
     {
+        [Display(Name = "ID")]
+        [Required(ErrorMessage = "ID is required")]
+        public int ReviewID { get; set; }
 
+        [Display(Name = "Paper ID")]
+        public int? PaperID { get; set; }
+
+        [Display(Name = "Reviewer ID")]
+        public int? ReviewerID { get; set; }
+
+        [Display(Name = "Appropriateness of Topic")]
+        [Range(0, 10)]
+        public decimal? AppropriatenessOfTopic { get; set; }
+
+        [Display(Name = "Timeliness of Topic")]
+        [Range(0, 10)]
+        public decimal? TimelinessOfTopic { get; set; }
+
+        [Display(Name = "Supportive Evidence")]
+        [Range(0, 10)]
+        public decimal? SupportiveEvidence { get; set; }
+
+        [Display(Name = "Technical Quality")]
+        [Range(0, 10)]
+        public decimal? TechnicalQuality { get; set; }
+
+        [Display(Name = "Scope of Coverage")]
+        [Range(0, 10)]
+        public decimal? ScopeOfCoverage { get; set; }
+
+        [Display(Name = "Citation of Previous Work")]
+        [Range(0, 10)]
+        public decimal? CitationOfPreviousWork { get; set; }
+
+        [Display(Name = "Originality")]
+        [Range(0, 10)]
+        public decimal? Originality { get; set; }
+
+        [Display(Name = "Comments on Content")]
+        public string? ContentComments { get; set; }
+
+        [Display(Name = "Organization of Paper")]
+        [Range(0, 10)]
+        public decimal? OrganizationOfPaper { get; set; }
+
+        [Display(Name = "Clarity of Main Message")]
+        [Range(0, 10)]
+        public decimal? ClarityOfMainMessage { get; set; }
+
+        [Display(Name = "Mechanics")]
+        [Range(0, 10)]
+        public decimal? Mechanics { get; set; }
+
+        [Display(Name = "Comments on Written Document")]
+        public string? WrittenDocumentComments { get; set; }
+
+        [Display(Name = "Suitability for Presentation")]
+        [Range(0, 10)]
+        public decimal? SuitabilityForPresentation { get; set; }
+
+        [Display(Name = "Potential Interest in Topic")]
+        [Range(0, 10)]
+        public decimal? PotentialInterestInTopic { get; set; }
+
+        [Display(Name = "Comments on Potential for Oral s")]
+        public string? PotentialForOralPresentationComments { get; set; }
+
+        [Display(Name = "Overall Rating")]
+        [Range(0, 10)]
+        public decimal? OverallRating { get; set; }
+
+        [Display(Name = "Comments on Overall Rating")]
+        public string? OverallRatingComments { get; set; }
+
+        [Display(Name = "Comfort Level Topic")]
+        [Range(0, 10)]
+        public decimal? ComfortLevelTopic { get; set; }
+
+        [Display(Name = "Comfort Level Acceptability")]
+        [Range(0, 10)]
+        public decimal? ComfortLevelAcceptability { get; set; }
+
+        [Display(Name = "Complete")]
+        public bool? Complete { get; set; }
+
+        public ReviewModel()
+        {
+            ReviewID                    = 0;
+            PaperID                     = 0;
+            ReviewerID                  = 0;    
+            AppropriatenessOfTopic      = null;
+            TimelinessOfTopic           = null;
+            SupportiveEvidence          = null;
+            TechnicalQuality            = null;
+            ScopeOfCoverage             = null;
+            CitationOfPreviousWork      = null;
+            Originality                 = null;
+            ContentComments             = null;
+            OrganizationOfPaper         = null;
+            ClarityOfMainMessage        = null;
+            Mechanics                   = null;
+            WrittenDocumentComments     = null;
+            SuitabilityForPresentation  = null;
+            PotentialInterestInTopic    = null;
+            PotentialForOralPresentationComments = null;
+            OverallRating               = null;
+            OverallRatingComments       = null;
+            ComfortLevelTopic           = null;
+            ComfortLevelAcceptability   = null;
+            Complete                    = null;
+        }
     }
 }
